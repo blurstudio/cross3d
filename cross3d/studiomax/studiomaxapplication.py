3c4677918b921f381a29764d8bad99bd63fe4791
johnk <johnk@896c8824-d3f5-0310-9c61-ac502b9a79dc>
2012-01-26 19:19:11 +0000

    git-svn-id: svn://svn.blur.com/blurdev/trunk/code/python/lib/blur3d/api@11590 896c8824-d3f5-0310-9c61-ac502b9a79dc

--- cross3d/studiomax/studiomaxapplication.py
+++ cross3d/studiomax/studiomaxapplication.py
@@ -204,6 +204,7 @@ class StudiomaxApplication(AbstractApplication):
 				'10000': "Max10",
 				'11000': "Max2009",
 				'12000': "Max2010",
+				'14000': "Max2012",
 				'default': "Max2010"}
 		if jobTypeDic.has_key(str(version[0])):
 			jobType = jobTypeDic[str(version[0])]
