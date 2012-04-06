82f01c975358a1b767c17c0892080686565d44d0
dougl <dougl@896c8824-d3f5-0310-9c61-ac502b9a79dc>
2012-04-06 19:51:25 +0000

    Fixed encoding quality for FileSequence class.
    git-svn-id: svn://svn.blur.com/blurdev/trunk/code/python/lib/blur3d/api@11836 896c8824-d3f5-0310-9c61-ac502b9a79dc

--- cross3d/classes/filesequence.py
+++ cross3d/classes/filesequence.py
@@ -170,7 +170,7 @@ class FileSequence( object ):
 				while not normalisedSequence.isComplete():
 					continue
 				import subprocess
-				command = [ ffmpeg, '-r', str( fps ), "-i", normalisedSequence.codePath(), '-vcodec', 'mjpeg', '-vb', '20000k', '-y', outputPath ]
+				command = [ ffmpeg, '-r', str( fps ), "-i", normalisedSequence.codePath(), '-vcodec', 'mjpeg', '-qscale', '1', '-y', outputPath ]
 				process = subprocess.Popen( command )
 				process.communicate()
 				normalisedSequence.delete()
