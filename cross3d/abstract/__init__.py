91950bc40ac36c7706483401f4d9a9111615a089
jbee <jbee@896c8824-d3f5-0310-9c61-ac502b9a79dc>
2013-06-04 00:22:48 +0000

    Adds import for abstractscenepropset.
    git-svn-id: svn://svn.blur.com/blurdev/trunk/code/python/lib/blur3d/api@13892 896c8824-d3f5-0310-9c61-ac502b9a79dc

--- cross3d/abstract/__init__.py
+++ cross3d/abstract/__init__.py
@@ -37,6 +37,7 @@ def init():
 	import abstractsceneanimationkey
 	import abstractscenecache
 	import abstractscenesubmitter
+	import abstractscenepropset
 	import abstractalembic
 	import abstractwritejob
 	import abstractwritejobs
