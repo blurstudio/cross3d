6b194c7ce14b572b9925bb367625c9d7d50774a2
dougl <dougl@896c8824-d3f5-0310-9c61-ac502b9a79dc>
2014-02-25 21:40:48 +0000

    Fixed a SceneModel methods that was causing exception with local models.
    git-svn-id: svn://svn.blur.com/blurdev/trunk/code/python/lib/blur3d/api@16096 896c8824-d3f5-0310-9c61-ac502b9a79dc

--- cross3d/softimage/softimagescenemodel.py
+++ cross3d/softimage/softimagescenemodel.py
@@ -102,11 +102,13 @@ class SoftimageSceneModel(AbstractSceneModel):
 		return False
 
 	def resolution(self):
-		
-		# If I dont re-initialize a model object it does not return me the right resolutions.
-		resolutions = self._scene.findObject(self.name()).resolutions()
-		if resolutions:
-			return resolutions[self._nativePointer.Parameters('active_resolution').Value]
+		if self.isReferenced():
+
+			# If I dont re-initialize a model object it does not return me the right resolutions.
+			resolutions = self._scene.findObject(self.name()).resolutions()
+			if resolutions:
+				return resolutions[self._nativePointer.Parameters('active_resolution').Value]
+				
 		return ''
 
 	def resolutions(self):
