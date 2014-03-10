5c34642a1974449a02921aa0948e0aad163f3c2d
brendana <brendana@896c8824-d3f5-0310-9c61-ac502b9a79dc>
2014-03-10 13:41:39 +0000

    added external for studiomax to blur3d
    git-svn-id: svn://svn.blur.com/blurdev/trunk/code/python/lib/blur3d/api@16248 896c8824-d3f5-0310-9c61-ac502b9a79dc

---
+++ cross3d/studiomax/external.py
@@ -0,0 +1,65 @@
+
+import os
+import sys
+import shutil
+import subprocess
+
+from blur3d.api.abstract.external import External as AbstractExternal
+
+#------------------------------------------------------------------------------------------------------------------------
+
+class External(AbstractExternal):
+
+	_architectureTokens = {64: r'_x64', 32: r''}
+	_versionTokens = {2012: r'Max2012'}
+
+	@classmethod
+	def runScript(cls, script, version=None, architecture=64):
+
+		if os.path.exists(script):
+			scriptPath = script
+
+		else:
+			scriptPath = cls.scriptPath()
+			fle = open(scriptPath, "w")
+			fle.write(script)
+			fle.close()
+
+		binary = os.path.join(cls.binariesPath(version, architecture), '3dsmax.exe')
+		process = subprocess.Popen([binary, '-U', 'MAXScript', scriptPath], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+		
+		# Writing the log file.
+		fle = open(cls.scriptLog(), 'w')
+		fle.write(process.stdout.read())
+		fle.close()
+
+		# Checking the error in the log file.
+		fle = open(cls.scriptLog())
+		content = fle.read()
+
+		return False if 'FATAL' in content else True
+
+	@classmethod
+	def scriptPath(cls):
+		return r'C:\temp\studiomax_script.py'
+
+	@classmethod
+	def scriptLog(cls):
+		return r'C:\temp\studiomax_script.log'
+
+	@classmethod
+	def binariesPath(cls, version=None, architecture=64):
+		
+		# Getting the latest version if not provided.
+		version = version or sorted(cls._versionTokens.keys())[-1]
+
+		# Getting tokens for the the binary folder path.
+		tokens = {}
+		tokens['architecture'] = cls._architectureTokens.get(architecture)
+		tokens['version'] = cls._versionTokens.get(version)
+
+		if tokens['architecture'] and tokens['version']:
+			return r'C:\{version}{architecture}\3ds Max 2012'.format(**tokens)
+
+		else:
+			raise Exception('Invalid version or architecture.')
