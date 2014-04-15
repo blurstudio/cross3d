d324586c3f41be2be9038d65dd07d45676e7e984
mikeh <mikeh@896c8824-d3f5-0310-9c61-ac502b9a79dc>
2014-04-15 23:02:05 +0000

    implemented blur3d.api.Scene().saveFileAs for Motion Builder. #12325
    git-svn-id: svn://svn.blur.com/blurdev/trunk/code/python/lib/blur3d/api@16724 896c8824-d3f5-0310-9c61-ac502b9a79dc

--- cross3d/motionbuilder/motionbuilderscene.py
+++ cross3d/motionbuilder/motionbuilderscene.py
@@ -8,14 +8,31 @@
 #	\date		06/21/12
 #
 
+import pyfbsdk as mob
 from blur3d.api.abstract.abstractscene import AbstractScene
+from PyQt4.QtGui import QFileDialog
 
 #------------------------------------------------------------------------------------------------------------------------
 
 class MotionBuilderScene( AbstractScene ):
 	def __init__( self ):
+		self._fbapp = mob.FBApplication()
 		AbstractScene.__init__( self )
-	
+
+	def saveFileAs(self, filename=''):
+		"""
+		Implements AbstractScene.saveFileAs to save the current scene to the inputed name specified.  If no name is supplied, 
+		then the user should be prompted to pick a filename
+		:param filename: Name of the file to save
+		:return	: Success, Bool
+		"""		
+		if not filename:
+			filename = unicode(QFileDialog.getSaveFileName(None, 'Save Motion Builder File', '', 'FBX (*.fbx);;All files (*.*)'))
+		print 'filename', filename, self._fbapp
+		if filename:
+			return self._fbapp.FileSave(unicode(filename).encode('utf8'))
+		return False
+
 # register the symbol
 from blur3d import api
 api.registerSymbol( 'Scene', MotionBuilderScene )
