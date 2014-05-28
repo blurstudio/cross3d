448ac719eb69bb8529957291904b03465318a2b8
brendana <brendana@896c8824-d3f5-0310-9c61-ac502b9a79dc>
2014-05-28 21:58:06 +0000

    Updated to support white balance and distortion amount equation diffing and syncing from trax.
    git-svn-id: svn://svn.blur.com/blurdev/trunk/code/python/lib/blur3d/api@17133 896c8824-d3f5-0310-9c61-ac502b9a79dc

--- cross3d/studiomax/studiomaxscenecamera.py
+++ cross3d/studiomax/studiomaxscenecamera.py
@@ -18,7 +18,7 @@ from blur3d.api.abstract.abstractscenecamera import AbstractSceneCamera
 
 #------------------------------------------------------------------------------------------------------------------------
 
-class StudiomaxSceneCamera( AbstractSceneCamera ):
+class StudiomaxSceneCamera(AbstractSceneCamera):
 	
 	_outputTypes = ['Still', 'Movie', 'Video']
 	_whiteBalances = ['Custom', 'Neutral', 'Daylight', 'D75', 'D65', 'D55', 'D50', 'Temperature']
@@ -81,16 +81,16 @@ class StudiomaxSceneCamera( AbstractSceneCamera ):
 		distance = sqrt((hypoLength * hypoLength) - ((axisLength / 2.0) * (axisLength / 2.0)))
 		cam.pos = (cam.pos + mxs.point3(0, -distance, 0))
 
-	def cameraType( self ):
+	def cameraType(self):
 		"""
 			\remarks	implements the AbstractSceneCamera.cameraType method to determine what type of camera this instance is
 			\return		<blur3d.api.constants.CameraType>
 		"""
 		cls = mxs.classof(self._nativePointer)
-		if ( cls in (mxs.FreeCamera,mxs.TargetCamera) ):
+		if (cls in (mxs.FreeCamera, mxs.TargetCamera)):
 			return CameraType.Standard
 		
-		elif ( cls == mxs.VRayPhysicalCamera ):
+		elif (cls == mxs.VRayPhysicalCamera):
 			return CameraType.VRayPhysical
 		return 0
 
@@ -115,7 +115,7 @@ class StudiomaxSceneCamera( AbstractSceneCamera ):
 			return int(round(fov))
 		return fov
 		
-	def lens( self, filmWidth=None, rounded=False):
+	def lens(self, filmWidth=None, rounded=False):
 		"""
 			\remarks	returns the current lens of the camera.
 			\return		<float>
@@ -124,21 +124,21 @@ class StudiomaxSceneCamera( AbstractSceneCamera ):
 			fov = math.radians(self.fov())
 			lens = (0.5 * float(filmWidth)) / math.tan(fov / 2.0)
 		else:
-			lens = mxs.cameraFOV.FOVtoMM( self.nativePointer().fov )
+			lens = mxs.cameraFOV.FOVtoMM(self.nativePointer().fov)
 		if rounded:
 			return int(round(lens))
 		return lens
 		
-	def hasMultiPassEffects( self ):
+	def hasMultiPassEffects(self):
 		"""
 			\remarks	returns whether multipass effects are active on this camera
 			\return		<bool>
 		"""
-		if mxs.isProperty( self._nativePointer, 'mpassEnabled' ):
+		if mxs.isProperty(self._nativePointer, 'mpassEnabled'):
 			return self._nativePointer.mpassEnabled
 		return False
 		
-	def renderMultiPassEffects( self ):
+	def renderMultiPassEffects(self):
 		"""
 			\remarks	runs the multipass effects on this camera
 			\return		<bool>
@@ -146,7 +146,7 @@ class StudiomaxSceneCamera( AbstractSceneCamera ):
 		mxs.maxOps.displayActiveCameraViewWithMultiPassEffect()
 		return True
 		
-	def generateRender( self, **options ):
+	def generateRender(self, **options):
 		"""
 			\remarks	renders an image sequence form that camera with the current render settings
 			\param 		path <String>
@@ -158,23 +158,23 @@ class StudiomaxSceneCamera( AbstractSceneCamera ):
 			\return		<blur3d.api.constants.CameraType>
 		"""
 		
-		path = options.get( 'path', '' )
-		resolution = options.get( 'resolution', QSize( mxs.renderWidth, mxs.renderHeight ) )
-		pixelAspect = options.get( 'pixelAspect', 1.0 )
-		step = options.get( 'step', 1 )
-		frameRange = options.get( 'frameRange', [] )
-		missingFramesOnly = options.get( 'missingFramesOnly', False )
+		path = options.get('path', '')
+		resolution = options.get('resolution', QSize(mxs.renderWidth, mxs.renderHeight))
+		pixelAspect = options.get('pixelAspect', 1.0)
+		step = options.get('step', 1)
+		frameRange = options.get('frameRange', [])
+		missingFramesOnly = options.get('missingFramesOnly', False)
 			
 		if path:
 			basePath = os.path.split(path)[0]
-			if not os.path.exists( basePath ):
-				os.makedirs( basePath )
+			if not os.path.exists(basePath):
+				os.makedirs(basePath)
 		
 		if frameRange:
-			bitmap = mxs.render( outputFile=path, fromFrame=frameRange[0], toFrame=frameRange[1], camera=self._nativePointer, nthFrame=step, outputWidth=resolution.width(), outputHeight=resolution.height(), pixelAspect=pixelAspect )
-			mxs.undisplay( bitmap )
+			bitmap = mxs.render(outputFile=path, fromFrame=frameRange[0], toFrame=frameRange[1], camera=self._nativePointer, nthFrame=step, outputWidth=resolution.width(), outputHeight=resolution.height(), pixelAspect=pixelAspect)
+			mxs.undisplay(bitmap)
 		else:
-			bitmap = mxs.render( outputFile=path, frame=mxs.pyHelper.namify( 'current' ), camera=self._nativePointer, outputWidth=resolution.width(), outputHeight=resolution.height(), pixelAspect=pixelAspect )	
+			bitmap = mxs.render(outputFile=path, frame=mxs.pyHelper.namify('current'), camera=self._nativePointer, outputWidth=resolution.width(), outputHeight=resolution.height(), pixelAspect=pixelAspect)	
 
 	def setFilmWidth(self, width):
 		"""
@@ -285,8 +285,15 @@ class StudiomaxSceneCamera( AbstractSceneCamera ):
 			self._nativePointer.Distortion = distortion
 			return True
 		return False
+	
+	def whiteBalancePreset(self):
+		return self._nativePointer.whiteBalance_preset
+	
+	def setWhiteBalancePreset(self, preset):
+		self._nativePointer.whiteBalance_preset = preset
+		return True
 
 	
 # register the symbol
 from blur3d import api
-api.registerSymbol( 'SceneCamera', StudiomaxSceneCamera )
+api.registerSymbol('SceneCamera', StudiomaxSceneCamera)
