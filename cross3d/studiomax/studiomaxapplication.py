64a21ae9d956233b0bdc6c6137fdfe5e9927f36c
mikeh <mikeh@896c8824-d3f5-0310-9c61-ac502b9a79dc>
2015-04-13 18:50:01 +0000

    Between the 'sceneOpenRequested' and 'sceneOpenFinished' callbacks StudiomaxApplication now blocks all signals. Reworked changes to StudiomaxApplication making sceneMergeFinished only emit once even if its called multiple times into a reusable format. #21325
    git-svn-id: svn://svn.blur.com/blurdev/trunk/code/python/lib/blur3d/api@19541 896c8824-d3f5-0310-9c61-ac502b9a79dc

--- cross3d/studiomax/studiomaxapplication.py
+++ cross3d/studiomax/studiomaxapplication.py
@@ -17,8 +17,10 @@
 
 from blur3d.api.abstract.abstractapplication import AbstractApplication
 from Py3dsMax import mxs
-from blurdev import debug, enum
+from blurdev import debug
+from blurdev.enum import EnumGroup, Enum
 from PyQt4.QtCore import QTimer
+_n = mxs.pyhelper.namify
 dispatch = None
 
 # initialize callback scripts
@@ -27,15 +29,14 @@ global blur3d
 if ( blur3d == undefined ) do ( blur3d = python.import "blur3d" )
 if ( blur3d != undefined ) do ( 
 	local ms_args = (callbacks.notificationParam())
-	blur3d.api.dispatch.%(function)s "%(signal)s" %(args)s 
+	blur3d.api.%(cls)s.%(function)s "%(signal)s" %(args)s 
 )
 """
 _STUDIOMAX_CALLBACK_TEMPLATE_NO_ARGS = """
 global blur3d
-print "Calling maxscript no args %(function)s, |%(signal)s|"
 if ( blur3d == undefined ) do ( blur3d = python.import "blur3d" )
 if ( blur3d != undefined ) do ( 
-	blur3d.api.dispatch.%(function)s "%(signal)s"
+	blur3d.api.%(cls)s.%(function)s "%(signal)s"
 )
 """
 _STUDIOMAX_VIEWPORT_TEMPLATE = """
@@ -43,38 +44,40 @@ fn blurfn_%(signal)s =
 (
 	if ( blur3d == undefined ) do ( blur3d = python.import "blur3d" )
 	if ( blur3d != undefined ) do ( 
-		blur3d.api.dispatch.%(function)s "%(signal)s"
+		blur3d.api.%(cls)s.%(function)s "%(signal)s"
 	)
 )
 """
 
-_STUDIOMAX_SCENEMERGE_REQUESTED_TEMPLATE = """
-global blur3d
-if ( blur3d == undefined ) do ( blur3d = python.import "blur3d" )
-if ( blur3d != undefined ) do ( 
-	blur3d.api.application._sceneMergeRequested()
-)
-"""
-
-_STUDIOMAX_SCENEMERGE_FINISHED_TEMPLATE = """
-global blur3d
-if ( blur3d == undefined ) do ( blur3d = python.import "blur3d" )
-if ( blur3d != undefined ) do ( 
-	blur3d.api.application._sceneMergeFinished()
-)
-"""
+class _ConnectionType(EnumGroup):
+	General = Enum()
+	Viewport = Enum()
 
 class _ConnectionDef:
-	ConnectionType = enum.enum('General', 'Viewport')
-	def __init__(self, signal, callback, arguments='', function='dispatch', callbackType=ConnectionType.General):
+	""" Class that stores all neccissary info to connect blur3d.api.dispatch to StudioMax
+	
+	Args:
+		signal str: The name of the blur3d.api.dispatch signal.
+		callback str: The name of StudioMax callback.
+		arguments str: A string of maxscript arguments passed to signal when the callback is emitted.
+		function str: The name of the blur3d.api.dispatch function. Used in the callback maxscript.
+		callbackType _ConnectionType: Controls how the callback is connected to StudioMax.
+		cls str: The name of the class called in blur3d.api. Normally 'dispatch'.
+		associated _ConnectionDef: When this signal is connected, all _ConnectionDef's in this list
+			are also connected to. This is used by filePostMerge to disable all callbacks durring
+			the opening of a file.
+	"""
+	def __init__(self, signal, callback, arguments='', function='dispatch', callbackType=_ConnectionType.General, cls='dispatch', associated=[]):
 		self.signal = signal
 		self.callback = callback
 		self.arguments = arguments
 		self.function = function
 		self.callbackType = callbackType
+		self.cls = cls
+		self.associated = associated
 
 	@staticmethod
-	def asDict(signal, callback, arguments = '', function = 'dispatch', callbackType=ConnectionType.General):
+	def asDict(signal, callback, arguments = '', function = 'dispatch', callbackType=_ConnectionType.General):
 		return {signal:_ConnectionDef(signal, callback, arguments, function, callbackType)}
 
 class _ConnectionStore(object):
@@ -95,12 +98,44 @@ class StudiomaxApplication(AbstractApplication):
 	_connectionMap = _ConnectionStore()
 	_connectionMap.update(_ConnectionDef('sceneNewRequested', 'systemPreNew'))
 	_connectionMap.update(_ConnectionDef('sceneNewFinished', 'systemPostNew'))
-	_connectionMap.update(_ConnectionDef('sceneOpenRequested', 'filePreOpen', '""'))
-	_connectionMap.update(_ConnectionDef('sceneOpenFinished', 'filePostOpen', '""'))
-	_connectionMap.update(_ConnectionDef('sceneMergeRequested', 'filePreMerge'))
+	_connectionMap.update(
+		_ConnectionDef(
+			'sceneOpenRequested', 
+			'filePreOpen', 
+			'""', 
+			function='_prePostCallback', 
+			cls='application'
+		)
+	)
+	_connectionMap.update(
+		_ConnectionDef(
+			'sceneOpenFinished', 
+			'filePostOpen', 
+			'""', 
+			function='_prePostCallback', 
+			cls='application', 
+			associated=_connectionMap.getConnectionsBySignalName('sceneOpenRequested')
+		)
+	)
+	_connectionMap.update(
+		_ConnectionDef(
+			'sceneMergeRequested', 
+			'filePreMerge', 
+			function='_prePostCallback', 
+			cls='application'
+		)
+	)
 	_connectionMap.update(_ConnectionDef('sceneReferenceRequested', 'objectXrefPreMerge'))
 	_connectionMap.update(_ConnectionDef('sceneReferenceRequested', 'sceneXrefPreMerge'))
-	_connectionMap.update(_ConnectionDef('sceneMergeFinished', 'filePostMerge'))
+	_connectionMap.update(
+		_ConnectionDef(
+			'sceneMergeFinished', 
+			'filePostMerge', 
+			function='_prePostCallback', 
+			cls='application', 
+			associated=_connectionMap.getConnectionsBySignalName('sceneMergeRequested')
+		)
+	)
 	_connectionMap.update(_ConnectionDef('sceneReferenceFinished', 'objectXrefPostMerge'))
 	_connectionMap.update(_ConnectionDef('sceneReferenceFinished', 'sceneXrefPostMerge'))
 	_connectionMap.update(_ConnectionDef('sceneSaveRequested', 'filePreSave', '(if (ms_args != undefined) then (ms_args as string) else "")'))
@@ -125,50 +160,90 @@ class StudiomaxApplication(AbstractApplication):
 	_connectionMap.update(_ConnectionDef('objectPostDelete', 'nodePostDelete', function = 'postDelete'))
 	_connectionMap.update(_ConnectionDef('objectParented', 'nodeLinked', 'ms_args', 'dispatchObject'))
 	_connectionMap.update(_ConnectionDef('objectUnparented', 'nodeUnlinked', 'ms_args', 'dispatchObject'))
-	_connectionMap.update(_ConnectionDef('viewportRedrawn', '', function='dispatchFunction', callbackType=_ConnectionDef.ConnectionType.Viewport))
+	_connectionMap.update(_ConnectionDef('viewportRedrawn', '', function='dispatchFunction', callbackType=_ConnectionType.Viewport))
 	
 	def __init__(self):
 		super(StudiomaxApplication, self).__init__()
 		self._sceneMergeFinishedTimer = QTimer(self)
 		self._sceneMergeFinishedTimer.setSingleShot(True)
 		self._sceneMergeFinishedTimer.timeout.connect(self._sceneMergeFinishedTimeout)
-	
-	def _sceneMergeFinished(self):
-		self._sceneMergeFinishedTimer.start(1000)
-	
-	def _sceneMergeRequested(self):
-		self._sceneMergeFinishedTimer.stop()
-	
-	def _sceneMergeFinishedTimeout(self):
-		dispatch.dispatch('sceneMergeFinished')
+		# Variable used to prevent emiting signals when a file is being opened.
+		self._openingScene = False
+		self._disconnectNames = set()
 	
 	def _connectStudiomaxSignal(self, connDef, blurdevSignal):
 		"""
 			\remarks	Responsible for connecting a signal to studiomax
 		"""
 		# store the maxscript methods needed
-		_n = mxs.pyhelper.namify
-		if connDef.callbackType == connDef.ConnectionType.Viewport:
-			signal = _STUDIOMAX_VIEWPORT_TEMPLATE % {'function':connDef.function, 'signal':blurdevSignal}
-			# Ensure that if the old signal existed it is removed before redefining it. If function is undefined it will do nothing
+		if connDef.callbackType == _ConnectionType.Viewport:
+			signal = _STUDIOMAX_VIEWPORT_TEMPLATE % {
+				'cls':'dispatch', 
+				'function':connDef.function, 
+				'signal':blurdevSignal
+			}
+			# Ensure that if the old signal existed it is removed before redefining it. 
+			# If function is undefined it will do nothing
 			mxs.unregisterRedrawViewsCallback(getattr(mxs, 'blurfn_%s' % blurdevSignal))
 			mxs.execute(signal)
 			mxs.registerRedrawViewsCallback(getattr(mxs, 'blurfn_%s' % blurdevSignal))
 		else:
-			if blurdevSignal == 'sceneMergeFinished':
-				script = _STUDIOMAX_SCENEMERGE_FINISHED_TEMPLATE % { 'function':connDef.function, 'signal': blurdevSignal }
-				mxs.callbacks.addScript( _n(connDef.callback), script, id = _n('blur3dcallbacks_merge') )
-				reqDefs = self._connectionMap.getConnectionsBySignalName('sceneMergeRequested')
-				# NOTE: for loop is probably not needed as there should only be one connection
-				for reqDef in reqDefs:
-					script = _STUDIOMAX_SCENEMERGE_REQUESTED_TEMPLATE % { 'function':reqDef.function, 'signal': 'sceneMergeRequested' }
-					mxs.callbacks.addScript( _n(reqDef.callback), script, id = _n('blur3dcallbacks_merge') )
-				return
-			if connDef.arguments:
-				script = _STUDIOMAX_CALLBACK_TEMPLATE % { 'function':connDef.function, 'signal': blurdevSignal, 'args': connDef.arguments }
-			else:
-				script = _STUDIOMAX_CALLBACK_TEMPLATE_NO_ARGS % { 'function':connDef.function, 'signal': blurdevSignal }
-			mxs.callbacks.addScript( _n(connDef.callback), script, id = _n('blur3dcallbacks') )
+			# Connec the callback
+			self._addCallback(connDef, blurdevSignal)
+			# Connect any associated callbacks using a diffrent ID name allows us to disconnect
+			# this signal without affecting any direct connections to the associated callbacks
+			for reqDef in connDef.associated:
+				self._addCallback(reqDef, reqDef.signal, 'blur3dcallbacks_{}'.format(connDef.callback))
+	
+	def _addCallback(self, connDef, blurdevSignal, callbackName='blur3dcallbacks'):
+		if connDef.arguments:
+			script = _STUDIOMAX_CALLBACK_TEMPLATE % { 
+				'cls':connDef.cls, 
+				'function':connDef.function, 
+				'signal': blurdevSignal, 
+				'args': connDef.arguments
+			}
+		else:
+			script = _STUDIOMAX_CALLBACK_TEMPLATE_NO_ARGS % { 
+				'cls':connDef.cls, 
+				'function':connDef.function, 
+				'signal': blurdevSignal 
+			}
+		mxs.callbacks.addScript( _n(connDef.callback), script, id = _n(callbackName) )
+		self._disconnectNames.add(callbackName)
+	
+	def _prePostCallback(self, signal, *args):
+		""" Handle pre\post callbacks intelegently to reduce the number of callbacks.
+		"""
+		if self.shouldBlockSignal(signal, dispatch.signalsBlocked()):
+			# Ignore this callback if signals are blocked.
+			return
+		if signal == 'sceneMergeFinished':
+			# Don't just emit this callback. The timer is used to allow the application
+			# to call sceneMergeRequested if there is another merge request.
+			# This prevents a single merge action emitting 3 sceneMergeFinished callbacks
+			self._sceneMergeFinishedTimer.start(1000)
+			return
+		elif signal == 'sceneMergeRequested':
+			# Stop the timer because we started on a new merge.
+			# This prevents a single merge action emitting 3 sceneMergeFinished callbacks
+			self._sceneMergeFinishedTimer.stop()
+			return
+		elif signal == 'sceneOpenFinished':
+			# Re-Enable sending signals at a application level.
+			self._openingScene = False
+		elif signal == 'sceneOpenRequested':
+			# Disable signals at the application level. This prevents sceneMerge signals
+			# from being emitted durring the open process.
+			self._openingScene = True
+		# Emit the signal as normal.
+		dispatch.dispatch(signal, *args)
+		# Note: This code doesn't handle custom functions, so if its needed by this system
+		# it will have to be added.
+	
+	def _sceneMergeFinishedTimeout(self):
+		""" Emit the sceneMergeFinished when the timeout expires. """
+		dispatch.dispatch('sceneMergeFinished')
 	
 	def allowedCharacters(self):
 		return 'A-Za-z0-9_. /+*<>=|-'
@@ -203,17 +278,13 @@ class StudiomaxApplication(AbstractApplication):
 		"""
 		if signal in self._connectionMap.getSignalNames():
 			connections = self._connectionMap.getConnectionsBySignalName(signal)
-			namify = mxs.pyhelper.namify
 			for connDef in connections:
-				if connDef.callbackType == connDef.ConnectionType.Viewport:
+				if connDef.callbackType == _ConnectionType.Viewport:
 					mxs.unregisterRedrawViewsCallback(getattr(mxs, 'blurfn_%s' % connDef.signal))
-				elif connDef.signal == 'sceneMergeFinished':
-					# NOTE: This is a sort of hacky way to remove it
-					blurdevid 	= mxs.pyhelper.namify('blur3dcallbacks_merge')
-					mxs.callbacks.removeScripts(id = blurdevid)
-					return
 				else:
-					mxs.callbacks.removeScripts(namify(connDef.callback), id = namify('blur3dcallbacks'))
+					mxs.callbacks.removeScripts(_n(connDef.callback), id = _n('blur3dcallbacks'))
+					for reqDef in connDef.associated:
+						mxs.callbacks.removeScripts(_n(reqDef.callback), id = _n('blur3dcallbacks_{}'.format(connDef.callback)))
 		else:
 			debug.debugMsg('Disconnect: Signal %s has no signal map' % signal, debug.DebugLevel.Mid)
 	
@@ -222,11 +293,9 @@ class StudiomaxApplication(AbstractApplication):
 			\remarks	disconnect application specific callbacks to <blur3d.api.Dispatch>. This will be called when <blur3d.api.Dispatch> is deleted,
 						disconnect is called when the last <blur3d.api.Dispatch> signal is disconnected.
 		"""
-		# remove normal callbacks
-		blurdevid 	= mxs.pyhelper.namify('blur3dcallbacks')
-		mxs.callbacks.removeScripts(id = blurdevid)
-		blurdevid 	= mxs.pyhelper.namify('blur3dcallbacks_merge')
-		mxs.callbacks.removeScripts(id = blurdevid)
+		# remove all normal callbacks
+		for name in self._disconnectNames:
+			mxs.callbacks.removeScripts(id = _n(name))
 		self._sceneMergeFinishedTimer.stop()
 		# undefine the add callback function
 		mxs.blur3daddcallback = None
@@ -291,6 +360,28 @@ class StudiomaxApplication(AbstractApplication):
 		
 	def sceneFileExtension(self):
 		return 'max'
+
+	def shouldBlockSignal(self, signal, default):
+		""" Allows the Application to conditionally block a signal.
+		
+		Normally you should pass blur3d.api.dispatch.signalsBlocked() to default.
+		In general if default is True this method should just return True. This will
+		prevent unexpected signal emits when a script called
+		blur3d.api.dispatch.blockSignals(True) to block all signals.
+		
+		Args:
+			signal (str): The name of the signal to check if it should be blocked.
+			default (bool): Returned if signal doesn't match any requirements.
+		
+		Returns:
+			bool: If the signal should be blocked.
+		"""
+		if default:
+			# If signals are blocked assume all signals should be blocked
+			return default
+		if signal == 'sceneOpenFinished':
+			return False
+		return self._openingScene
 
 	def modelFileExtension(self):
 		return self.sceneFileExtension()
