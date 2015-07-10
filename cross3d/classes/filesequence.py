6fc557349b9f9d263728994f70852fadb5cf91a8
willc <willc@896c8824-d3f5-0310-9c61-ac502b9a79dc>
2015-07-10 23:11:02 +0000

    -> no longer reverses path before matching -> uses "." character to separate the extension, and looks for alphanumeric extensions of 1 or more characters. (Previously looking for 3 alphabetic character extensions.)
    git-svn-id: svn://svn.blur.com/blurdev/trunk/code/python/lib/blur3d/api@20008 896c8824-d3f5-0310-9c61-ac502b9a79dc

--- cross3d/classes/filesequence.py
+++ cross3d/classes/filesequence.py
@@ -75,12 +75,12 @@ class FileSequence(object):
 		return '%(baseName)s%(separator)s%(start)s-%(end)s.%(extension)s'
 
 	def nameTokens(self):
-		regex = re.compile(r'^((?P<extension>[A-Za-z][A-Za-z0-9][A-Za-z]).)((?P<range>(?P<end>[0-9]+)\-(?P<start>[0-9]+))(?P<separator>.))(?P<baseName>[A-Za-z0-9_.\-]+)$')
-		match = regex.match(self.name()[::-1])
+		regex = re.compile(r'^(?P<baseName>[A-Za-z0-9_.\-]+)((?P<separator>[^\da-zA-Z])(?P<range>(?P<start>[0-9]+)\-(?P<end>[0-9]+)))(\.(?P<extension>[a-zA-Z0-9]+))$')
+		match = regex.match( self.name() )
 		if match:
 			dict = match.groupdict()
 			for key in dict.keys():
-				dict[key] = dict[key][::-1]
+				dict[key] = dict[key]
 			return dict
 		return {}
 
