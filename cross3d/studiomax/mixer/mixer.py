77adf3437c24aedb8a081be1c5b8cfac01e28535
willc <willc@896c8824-d3f5-0310-9c61-ac502b9a79dc>
2015-10-17 01:47:30 +0000

    -> Remove getMixers, this functionality was moved to the class Scene as the method animationMixers()
    git-svn-id: svn://svn.blur.com/blurdev/trunk/code/python/lib/blur3d/api@20814 896c8824-d3f5-0310-9c61-ac502b9a79dc

--- cross3d/studiomax/mixer/mixer.py
+++ cross3d/studiomax/mixer/mixer.py
@@ -140,27 +140,6 @@ class StudiomaxMixer(AbstractMixer):
 		return ClipPortions
 
 ################################################################################
-#####----------------------------- Functions ------------------------------#####
-################################################################################
-
-def getMixers():
-	"""Finds all mixers present in the current MAX scene.
-
-	Returns:
-					list: A list of Mixer instances for each MxMixer in the
-						current MAX Scene.
-	"""
-	# Since Mixer is a Class in MAXScript, and not a MAXClass, we're not able
-	# to use getClassInstances to retrieve Mixers in the scene.  Instead, we'll
-	# iterate over objects in the scene, and test to see if they have a
-	# controller with a Mixer attached.
-	mixers = []
-	for obj in mxs.objects:
-		if hasattr(obj, 'controller') and hasattr(obj.controller, 'mixer'):
-			mixers.append(Mixer(obj.controller.mixer))
-	return mixers
-
-################################################################################
 
 # register the symbol
 from blur3d import api
